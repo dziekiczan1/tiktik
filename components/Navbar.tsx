d8e12d2d2bef46de6869765ddec136ee0c40fe0f
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-06 22:05:21 +0200

    navbar component styles added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,23 @@ import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="w-full flex justify-between item-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={logo}
+            alt="TikTik"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
