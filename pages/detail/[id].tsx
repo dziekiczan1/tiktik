362e12c7b2fbddc4b2cbe8b4ddfc42f1ddefccbc
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-20 19:45:51 +0200

    set up the jsx for video details page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -21,7 +21,29 @@ const Detail = ({ postDetails }: IProps) => {
 
   if (!post) return null;
 
-  return <div>Detail</div>;
+  return (
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
+        <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p>
+            <MdOutlineCancel className="text-white text-[35px]" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              ref={videoRef}
+              loop
+              onClick={() => {}}
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+            ></video>
+          </div>
+          <div>play</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const getServerSideProps = async ({
