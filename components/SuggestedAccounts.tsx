798a1597328387b560ddea79074df12fa5a04e25
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-01-19 18:06:22 +0100

    add user styles in Suggested Accounts

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -32,6 +32,15 @@ const SuggestedAccounts = () => {
                   alt="user profile"
                   layout="responsive"
                 />
+                <div className="hidden xl:block">
+                  <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                    {user.userName.replaceAll(" ", "")}
+                    <GoVerified className="text-blue-400" />
+                  </p>
+                  <p className="capitzalize text-gray-400 text-xs">
+                    {user.userName}
+                  </p>
+                </div>
               </div>
             </div>
           </Link>
