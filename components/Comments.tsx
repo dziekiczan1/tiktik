bf45305b604672906432ef10caab1c1f9df1f86f
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-01-19 18:19:11 +0100

    add comments style to Comments component

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import { GoVerified } from "react-icons/go";
 
 import useAuthStore from "../store/authStore";
 import NoResults from "./NoResults";
+import { IUser } from "../types";
 
 interface IProps {
   isPostingComment: Boolean;
@@ -28,13 +29,45 @@ const Comments = ({
   comments,
   isPostingComment,
 }: IProps) => {
-  const { userProfile } = useAuthStore();
+  const { userProfile, allUsers } = useAuthStore();
 
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
         {comments?.length ? (
-          <div>videos</div>
+          comments.map((item, idx) => (
+            <>
+              {allUsers.map(
+                (user: IUser) =>
+                  user._id === (item.postedBy._id || item.postedBy._ref) && (
+                    <div className="p-2 items-center" key={idx}>
+                      <Link href={`/profile/${user._id}`}>
+                        <div className="flex items-start gap-3">
+                          <div className="w-12 h-12">
+                            <Image
+                              width={48}
+                              height={48}
+                              className="rounded-full cursor-pointer"
+                              src={user.image}
+                              alt="user-profile"
+                              layout="responsive"
+                            />
+                          </div>
+
+                          <p className="flex cursor-pointer gap-1 items-center text-[18px] font-bold leading-6 text-primary">
+                            {user.userName}{" "}
+                            <GoVerified className="text-blue-400" />
+                          </p>
+                        </div>
+                      </Link>
+                      <div>
+                        <p>{item.comment}</p>
+                      </div>
+                    </div>
+                  )
+              )}
+            </>
+          ))
         ) : (
           <NoResults text="No comments yet!" />
         )}
--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -25,22 +25,23 @@ const SuggestedAccounts = () => {
             <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
               <div className="w-8 h-8">
                 <Image
-                  src={user.image}
                   width={34}
                   height={34}
                   className="rounded-full"
-                  alt="user profile"
+                  src={user.image}
+                  alt="user-profile"
                   layout="responsive"
                 />
-                <div className="hidden xl:block">
-                  <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
-                    {user.userName.replaceAll(" ", "")}
-                    <GoVerified className="text-blue-400" />
-                  </p>
-                  <p className="capitzalize text-gray-400 text-xs">
-                    {user.userName}
-                  </p>
-                </div>
+              </div>
+
+              <div className="hidden xl:block">
+                <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                  {user.userName.replace(/\s+/g, "")}{" "}
+                  <GoVerified className="text-blue-400" />
+                </p>
+                <p className="capitalize text-gray-400 text-xs">
+                  {user.userName}
+                </p>
               </div>
             </div>
           </Link>
